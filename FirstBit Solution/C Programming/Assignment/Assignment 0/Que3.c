27331758c3c596479dfcc0a9e58309493a82fcda
abhishek-arkas <abhishekarkas511@gmail.com>
2026-02-10 07:41:14 -0800

    Update Que3.c

--- FirstBit Solution/C Programming/Assignment/Assignment 0/Que3.c
+++ FirstBit Solution/C Programming/Assignment/Assignment 0/Que3.c
@@ -1,6 +1,8 @@
+//conversion of temperature from degree celcius to fahrenheite
 void main(){
 	float celsius,fahrenheit;
     fahrenheit=(celsius*9/5)+32;
 	printf("temperature in fahrenheit is %f",fahrenheit);
     
+
 }
