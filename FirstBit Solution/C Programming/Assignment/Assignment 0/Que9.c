6e6399ebf8ef1919ba07611fdb34f45bed350e70
abhishek-arkas <abhishekarkas511@gmail.com>
2026-02-10 07:45:41 -0800

    Update Que9.c

--- FirstBit Solution/C Programming/Assignment/Assignment 0/Que9.c
+++ FirstBit Solution/C Programming/Assignment/Assignment 0/Que9.c
@@ -1,7 +1,9 @@
+//Area of rectangle
 void main(){
 	float base=5,height=4,area;
 	area=0.5*base*height;
 	printf("base of the triangle %f\n height of the triangle %f\n",base,height);
 	printf("area of the triangle %f",area);
 	
+
 }
